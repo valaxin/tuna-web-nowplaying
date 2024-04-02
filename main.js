c01fe1c211b74248f053142bd77ed620552683d2
MacKenzie <mackenzie.bobbitt@outlook.com>
2024-04-02 17:57:18 -0300

    broken, need to revise and optimize

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 (async () => {
+  
   /* states: 0 = uninitalized, 1 = initalized, 2 = error  */
 
   const PORT = `5858`;
   const ENDPOINT = `http://localhost:${PORT}/`;
   const CACHE = { ENDPOINT, STATE: 0, LOG: [] };
   const RATE = 3200;
+  const TIMEOUT = 45000
+  const SELECTORS = [ '.stage', '.information-container', '.song-title', '.song-artist' ]
+
+  let tick = 0;
 
   // request the data presented at defined address:port by tuna obs plugin
   // expected data type is json.
@@ -20,11 +25,14 @@
 
   // initlize "CACHE" object by setting data "as is" into the object.
   async function setIntoCache(object, data) {
+
+    /*
     console.log([
       `Congrats! "object.STATE" exists and is set to 0=uninialized or 1=initalized state.`,
       (typeof object.STATE === "number" && object.STATE === 0) ||
         object.STATE === 1,
     ]);
+    */
 
     // proceed...
     if (
@@ -39,9 +47,12 @@
 
       // either there isn't a value at index 0 (undefined) or its some object notation. set or replcae the data
 
-      console.log(`now playing "${data.title}" by "${data.artists[0]}"`);
+      // console.info(`Tick Count ${tick++}`);
+
+      console.log(object)
 
       if (object.LOG[0] === undefined) {
+        object.LOG[0].timeout = Math.floor((object.LOG[0].duration - TIMEOUT))
         object.LOG.splice(0, 0, data);
         object.STATE = 1;
       }
@@ -55,19 +66,17 @@
         }
       };
 
-      // compare previouly mentioned ids, and prepend to log if change present.
+      // compare previouly mentioned ids if they match, add finised=true to index 0 then replace index 0 with new data, setting timeout for new on exit.
       if (object.LOG[0].title) {
         if (getSpotifyTrackID(object.LOG[0]) !== getSpotifyTrackID(data)) {
-          console.log(
-            `Hey buddy! Data says the song changed, let's get this switched up!`
-          );
+          console.info(`SONG CHANGED - ${data.title} - ${data.artists[0]}`)
           object.LOG[0].finished = true;
           object.LOG.splice(0, 0, data);
+          object.LOG[0].timeout = Math.floor((object.LOG[0].duration - TIMEOUT))
         }
       }
       return object;
     } else {
-        // return error
       object.STATE = 2;
       return {
         object,
@@ -76,10 +85,27 @@
     }
   }
 
+  // insert elements into the DOM, formated with classes as needed.
+  async function insertIntoDOM (selector, data) {
+    let hook = document.querySelector(selector)
+    hook.innerHTML = `
+      <div class="information-container">
+        <span class="song-title">${data.title}</span>
+        </br>
+        <span class="song-artist">${data.artists[0]}</span>
+      </div>
+    `
+    return hook.children[0]
+  }
+
   async function main() {
     setInterval(async () => {
       let jsondata = await requestCurrentSong(ENDPOINT, { Method: "GET" });
-      let cache = await setIntoCache(CACHE, jsondata);
+      let newcache = await setIntoCache(CACHE, jsondata);
+      let elements = await insertIntoDOM(SELECTORS[0], newcache.LOG[0])
+
+      console.log({ jsondata, newcache, elements })
+
     }, RATE);
   }
 
